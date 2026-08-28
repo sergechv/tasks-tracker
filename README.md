2f827cba6125f1967dd8be1c3ef8e19094c58665
sergechv <sergioj.chaidez@gmail.com>
2026-08-27 20:04:07 -0600

    Add: update and delete tasks

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# tasks-tracker
+# Tasks-tracker
+
+------
+
+simple CLI tasks tracker to create, list, update and delete tasks.
+
+------
+
+## Features
+
+- Add: Create a task with a description
+- Update: Update the status of a task by its ID
+- List: List all tasks or filter by status
+- Delete: Delete a task by its ID
+
+------
+
+## Installation
+
+```bash
+git clone https://github.com/sergechv/tasks-tracker
+cd tasks-tracker
+chmod +x main.py
+```
+
+------
+
+## Usage
+
+```bash
+$ ./main.py add "study python"
+$ ./main.py list {to-do, in-progress, done} # all by default
+$ ./main.py update id {to-do, in-progress, done}
+$ ./main.py delete id
+```
+
+------
+
+## License
+
+MIT License
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+import argparse
 import json
 from datetime import datetime
-import sys
 
 
 def load_tasks():
@@ -13,17 +13,33 @@ def load_tasks():
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
+
 def save_tasks(tasks):
     with open("tasks.json", "w") as f:
         json.dump(tasks, f, indent=4)
 
+
 def main():
     tasks = load_tasks()
+    parser = argparse.ArgumentParser(prog="Prog")
+    subparsers = parser.add_subparsers(dest="command")
+    add_parser = subparsers.add_parser("add", help="Add a task to the list of tasks")
+    add_parser.add_argument("description", type=str)
+    update_parser = subparsers.add_parser("update", help="Update the status of a task")
+    update_parser.add_argument("id", type=int)
+    update_parser.add_argument("status", choices=["to-do", "in-progress", "done"])
+    list_parser = subparsers.add_parser(
+        "list", help="list all tasks or filter by status"
+    )
+    list_parser.add_argument("-s", "--status", choices=["to-do", "in-progress", "done"])
+    delete_parser = subparsers.add_parser("delete", help="Delete a task by its id")
+    delete_parser.add_argument("id", type=int)
+    args = parser.parse_args()
+    dt = datetime.now().replace(microsecond=0).isoformat()
 
-    def add(tasks):
-        description = sys.argv[2]
+    def add(tasks, args):
+        description = args.description
         new_id = 0
-        dt = datetime.now().replace(microsecond=0).isoformat()
 
         if tasks:
             new_id = max(tasks.keys()) + 1
@@ -35,25 +51,53 @@ def main():
             "description": description,
             "status": "to-do",
             "createdAt": dt,
-            "updatedAt": dt
+            "updatedAt": dt,
         }
 
         tasks[new_id] = new_task
         save_tasks(tasks)
-        
+
         print(f'"{description}" succesfully added!')
-    
-    if sys.argv[1] == "add":
-        add(tasks)
 
-    def list_tasks(tasks):
-        if len(sys.argv) < 3:
-            tasks_list = list(tasks.values())
-            for item in tasks_list:
-                print(f'{item["description"]}')
+    if args.command == "add":
+        add(tasks, args)
 
-    if sys.argv[1] == "list":
-        list_tasks(tasks)
+    def list_tasks(tasks, args):
+        tasks_list = list(tasks.values())
+
+        for item in tasks_list:
+            if item["status"] == args.status or not args.status:
+                print(
+                    f"{item['description']} id: {item['id']} status: {item['status']}"
+                )
+
+    if args.command == "list":
+        list_tasks(tasks, args)
+
+    def update_list(tasks, args):
+        try:
+            tasks[args.id]["status"] = args.status
+            tasks[args.id]["updatedAt"] = dt
+            save_tasks(tasks)
+            print("Task updated")
+
+        except KeyError:
+            print(f"Invalid key, '{args.id}' does not exists")
+
+    if args.command == "update":
+        update_list(tasks, args)
+
+    def delete_tasks(tasks, args):
+        try:
+            del tasks[args.id]
+            save_tasks(tasks)
+            print("Tasks deleted")
+            raise KeyError
+        except KeyError:
+            print("Not a valid id")
+
+    if args.command == "delete":
+        delete_tasks(tasks, args)
 
 
 if __name__ == "__main__":
